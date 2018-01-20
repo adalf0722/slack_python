be40155ef4df48f2a73cba19d6a87c0bbd2070f7
Adalf <wuwenxian@adalf.local>
2018-01-20 20:04:46 +0800

    列出channel及member

---
+++ list_channels_and_members.py
@@ -0,0 +1,39 @@
+#-*- coding: utf8  -*-
+import os
+from slackclient import SlackClient
+
+slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+BOT_NAME = 'bot'
+USER = {}
+
+api_call = slack_client.api_call("users.list")
+if api_call.get('ok'):
+	# retrieve all users
+	users = api_call.get('members')
+	for user in users:
+		DESC = 'User'
+		if 'name' in user and BOT_NAME in user.get('name') :
+			DESC = 'Bot'
+		USER[str(user.get('id'))] = str(user['name'])
+		#print(DESC + " ID for '" + user['name'] + "' is " + user.get('id'))
+else:
+	print("could not find bot user with the name " + BOT_NAME)
+
+
+# Public
+print 'Public Channels...'
+api_call = slack_client.api_call("channels.list")
+for chn in api_call['channels']:
+	#print chn
+	print 'Public Channel:', chn['name'], chn['id']
+	for member in chn['members']:
+		print '*****member:', member, USER[str(member)]
+
+# Private
+print 'Private Channels...'
+api_call = slack_client.api_call("groups.list")
+for chn in api_call['groups']:
+	#print chn
+	print 'Private Channel:', chn['name'], chn['id']
+	for member in chn['members']:
+		print '*****member:', member, USER[str(member)]
